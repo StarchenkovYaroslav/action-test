b8cb7610a22a924e6b426234b185bf2ca837814f
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-12 18:04:34 +0600

    create pull

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,20 @@ async function main() {
     await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      ref: `refs/heads/${name}`,
+      ref: `refs/heads/prerelease-${name}`,
       sha: github.context.sha,
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
+
+    await octokit.request('POST /repos/{owner}/{repo}/pulls', {
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title: `Release-${name}`,
+      body: 'New release',
+      head: `prerelease-${name}`,
+      base: 'release',
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
