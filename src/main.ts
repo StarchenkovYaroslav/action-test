2cfa60567a5dfa84af267e45338a08e2bd6bcc2f
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-12 16:23:06 +0600

    create branch

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
+import * as github from '@actions/github'
 import * as core from '@actions/core'
 
 async function main() {
   try {
+    const name = core.getInput('Name')
+    const token = core.getInput('myToken')
+    const octokit = github.getOctokit(token)
 
-    const name = core.getInput('Name');
-    core.info(`Hello, ${name}`);
+    await octokit.request('POST /repos/{owner}/{repo}/git/refs', {
+      owner: '{owner}',
+      repo: '{repo}',
+      ref: `refs/heads/${name}`,
+      sha: 'aa218f56b14c9653891f9e74264a383fa43fefbd',
+      headers: {
+        'X-GitHub-Api-Version': '2022-11-28'
+      }
+    })
 
   } catch (error) {
     core.setFailed('Ошибка')
