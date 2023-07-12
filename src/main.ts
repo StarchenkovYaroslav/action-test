e95c754154dad64a0637073cfbd8384e06392a6d
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2023-07-12 16:33:14 +0600

    add sha

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function main() {
       owner: '{owner}',
       repo: '{repo}',
       ref: `refs/heads/${name}`,
-      sha: 'aa218f56b14c9653891f9e74264a383fa43fefbd',
+      sha: '9a0ec64fc1036243cbae9b62510a089109ea7716',
       headers: {
         'X-GitHub-Api-Version': '2022-11-28'
       }
